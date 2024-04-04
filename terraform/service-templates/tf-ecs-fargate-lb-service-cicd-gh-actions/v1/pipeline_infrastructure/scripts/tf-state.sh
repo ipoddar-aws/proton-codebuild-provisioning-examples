56322b093a818683ada58a26e8e3bd17d6b44175
ipoddar-aws <115023273+ipoddar-aws@users.noreply.github.com>
2024-04-03 22:05:44 -0400

    Use local s3 bucket for tf-state.sh

--- terraform/service-templates/tf-ecs-fargate-lb-service-cicd-gh-actions/v1/pipeline_infrastructure/scripts/tf-state.sh
+++ terraform/service-templates/tf-ecs-fargate-lb-service-cicd-gh-actions/v1/pipeline_infrastructure/scripts/tf-state.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
-export TF_STATE_BUCKET=$(echo $IN | jq '.service_instances[0].environment.outputs.tf_state_bucket' -r)
+#export TF_STATE_BUCKET=$(echo $IN | jq '.service_instances[0].environment.outputs.tf_state_bucket' -r)
+export TF_STATE_BUCKET=proton-tf-state-bucket
 export PROTON_SVC=$(echo ${IN} | jq '.service.name' -r)
 export KEY=${PROTON_SVC}.pipeline.tfstate
